e358972a8bc0ee35d3a61c35bdb76e39d549ab03
randa1 <randa@gtechme.com>
2019-05-14 13:01:22 +0400

    cache

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@
 
 // CODELAB: Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v3';
-
+const DATA_CACHE_NAME = 'data-cache-v4';
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
   '/pwa',
@@ -79,18 +79,31 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
   // CODELAB: Add fetch event handler here.
-  if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
-  }
+  if (evt.request.url.includes('/pwa')) {
+  console.log('[Service Worker] Fetch (data)', evt.request.url);
+  evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            }).catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+      }));
+  return;
+}
   evt.respondWith(
       fetch(evt.request)
           .catch(() => {
             return caches.open(CACHE_NAME)
                 .then((cache) => {
-                  return cache.match('offline.html');
+                    return response || fetch(evt.request);
                 });
           })
   );
