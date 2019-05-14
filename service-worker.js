73ee1e59171ffc82b22b9e20218ce2ef5bb55665
randa1 <randa@gtechme.com>
2019-05-14 12:51:02 +0400

    cache

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,6 @@
 
 // CODELAB: Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v3';
-const DATA_CACHE_NAME = 'data-cache-v4';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
