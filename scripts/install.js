026942666b9e814ab5574b3dfdf22af05c1ffea9
randa1 <randa@gtechme.com>
2019-05-14 14:32:18 +0400

    rmv btn

--- scripts/install.js
+++ scripts/install.js
@@ -34,7 +34,7 @@ window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 function saveBeforeInstallPromptEvent(evt) {
   // CODELAB: Add code to save event & show the install button.
   deferredInstallPrompt = evt;
-  installButton.removeAttribute('hidden');
+  //installButton.removeAttribute('hidden');
 }
 
 
